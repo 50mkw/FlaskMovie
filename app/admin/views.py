6b17a82d4160072d024f391072a3294b6034d89a
50mkw <patrick.yang@50mkw.com>
2019-11-05 22:55:50 +0800

    Chapter eight

--- app/admin/views.py
+++ app/admin/views.py
@@ -16,3 +16,8 @@ def login():
 @admin.route("/logout/")
 def logout():
     return redirect(url_for("admin.login"))
+
+
+@admin.route("/pwd/")
+def pwd():
+    return render_template('admin/pwd.html')
