d7f2cc41ddae897085008ff63eb4ab7f6b0ce416
50mkw <patrick.yang@50mkw.com>
2019-11-05 22:18:01 +0800

    Chapter seven

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,6 +1,18 @@
 from . import admin
+from flask import render_template, redirect, url_for
+
 
 # 调用蓝图
 @admin.route("/")
 def index():
-    return "<h1 style='color:red'>后台</h1>"
+    return render_template('admin/index.html')
+
+
+@admin.route("/login/")
+def login():
+    return render_template('admin/login.html')
+
+
+@admin.route("/logout/")
+def logout():
+    return redirect(url_for("admin.login"))
