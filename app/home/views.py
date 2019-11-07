0c7e95cac4e5221c58f7d1dd3a47d6d3c99ddede
50mkw <patrick.yang@50mkw.com>
2019-11-07 13:12:40 +0800

    Chapter 25

--- app/home/views.py
+++ app/home/views.py
@@ -1,10 +1,24 @@
 from . import home
 from flask import render_template, redirect, url_for, flash, session, request
-from .forms import RegisterForm, LoginFrom
+from .forms import RegisterForm, LoginFrom, UserDetailForm, PwdForm
 from app.models import User, UserLog
 from werkzeug.security import generate_password_hash
-from app import db
+from app import db, app
 import uuid
+from functools import wraps
+from werkzeug.utils import secure_filename
+import os
+
+# 要求登录才能访问
+def user_login_require(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if session.get('login_user', None) is None:
+            # 如果session中未找到该键，则用户需要登录
+            return redirect(url_for('home.login', next=request.url))
+        return func(*args, **kwargs)
+
+    return decorated_function
 
 
 @home.route("/")
@@ -55,13 +69,76 @@ def register():
         return redirect(url_for('home.register'))
     return render_template('home/register.html', form=form)
 
-@home.route('/user/')
-def user():
-    return render_template('home/user.html')
 
-@home.route('/pwd/')
+@home.route('/user/', methods=['GET', 'POST'])
+@user_login_require
+def user():
+    login_user = User.query.get_or_404(int(session['login_user_id']))
+    form = UserDetailForm(
+        name=login_user.name,
+        email=login_user.email,
+        phone=login_user.phone,
+        info=login_user.info
+    )
+    form.face.validators = []
+    form.face.render_kw = {'required': False}
+    if form.validate_on_submit():
+        data = form.data
+        face_save_path = app.config['USER_IMAGE']
+        if not os.path.exists(face_save_path):
+            os.makedirs(face_save_path)  # 如果文件保存路径不存在，则创建一个多级目录
+            import stat
+            os.chmod(face_save_path, stat.S_IRWXU)  # 授予可读写权限
+
+        if form.face.data:
+            # 上传文件不为空保存
+            if login_user.face and os.path.exists(os.path.join(face_save_path, login_user.face)):
+                os.remove(os.path.join(face_save_path, login_user.face))
+            # 获取上传文件名称
+            file_face = secure_filename(form.face.data.filename)
+            # !!!AttributeError: 'str' object has no attribute 'filename'，前端需要加上enctype="multipart/form-data"
+            from app.admin.views import change_filename
+            login_user.face = change_filename(file_face)
+            form.face.data.save(face_save_path + login_user.face)
+
+        if login_user.name != data['name'] and User.query.filter_by(name=data['name']).count() == 1:
+            flash('昵称已经存在', 'err')
+            return redirect(url_for('home.user'))
+        login_user.name = data['name']
+
+        if login_user.email != data['email'] and User.query.filter_by(email=data['email']).count() == 1:
+            flash('邮箱已经存在', 'err')
+            return redirect(url_for('home.user'))
+        login_user.email = data['email']
+
+        if login_user.phone != data['phone'] and User.query.filter_by(phone=data['phone']).count() == 1:
+            flash('手机号已经存在', 'err')
+            return redirect(url_for('home.user'))
+        login_user.phone = data['phone']
+
+        login_user.info = data['info']
+
+        db.session.commit()
+        flash('修改资料成功', 'ok')
+        return redirect(url_for('home.user'))
+    return render_template('home/user.html', form=form, login_user=login_user)
+
+@home.route('/pwd/', methods=['GET', 'POST'])
+@user_login_require
 def pwd():
-    return render_template('home/pwd.html')
+    login_user = User.query.get_or_404(int(session['login_user_id']))
+    form = PwdForm()
+    if form.validate_on_submit():
+        data = form.data
+        if login_user.check_pwd(data['oldpwd']):
+            login_user.pwd = generate_password_hash(data['newpwd'])
+            db.session.commit()
+            flash('密码修改成功，请重新登录', category='ok')
+            return redirect(url_for('home.login'))
+        else:
+            flash('旧密码不正确', category='err')
+            return redirect(url_for('home.pwd'))
+    return render_template('home/pwd.html', form=form)
 
 @home.route('/comments/')
 def comments():
